19115b5064e110de6b03ea5efeb4c7726b841806
tatsubow <tatsuhiro.takada@techouse.jp>
2025-10-15 19:22:20 +0900

    add controller_tas(json)#create

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -15,4 +15,31 @@ RSpec.describe TasksController, type: :controller do
             expect(response.content_type).to include "application/json"
         end
     end
+
+    describe "#create" do
+        it "responds with JSON formatted output" do
+            new_task = { name: "New test task" }
+            sign_in @user
+            post :create, format: :json,
+             params: { project_id: @project.id, task: new_task}
+            expect(response.content_type).to include "application/json"
+        end
+
+        it "adds a new task to the project" do
+            new_task = { name: "New test task" }
+            sign_in @user
+            expect {
+                post :create, format: :json,
+                 params: { project_id: @project.id, task: new_task}
+             }.to change(@project.tasks, :count).by(1)
+        end
+
+        it "requires authentication" do
+            new_task = { name: "New test task" }
+            expect {
+                post :create, format: :json,
+                 params: { project_id: @project.id, task: new_task}
+            }.to_not change(@project.tasks, :count)
+        end
+    end
 end

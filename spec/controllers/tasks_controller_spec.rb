10611e4aeab033803c1a33e204f9cfb5d3364966
tatsubow <tatsuhiro.takada@techouse.jp>
2025-10-16 19:45:57 +0900

    custom matcher

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
     include_context "project setup"
-    
+
     describe "#show" do
         it "responds with JSON formatted output" do
             sign_in user
             get :show, format: :json,
              params: { project_id: project.id, id: task.id }
-            expect(response.content_type).to include "application/json"
+            expect(response).to have_content_type(:json)
         end
     end
 
@@ -18,7 +18,7 @@ RSpec.describe TasksController, type: :controller do
             sign_in user
             post :create, format: :json,
              params: { project_id: project.id, task: new_task}
-            expect(response.content_type).to include "application/json"
+            expect(response).to have_content_type(:json)
         end
 
         it "adds a new task to the project" do
